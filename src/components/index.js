873d0b78c320d45a5564bd2d4a261448cf8a8ac7
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-09 01:58:18 +0500

    About init

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar";
 import LandingPage from "./LandingPage";
 import Marquee from "./Marquee";
-
+import About from "./About";
 
 
 
 export {
     Navbar,
     LandingPage,
-    Marquee
+    Marquee,
+    About,
+
 }
