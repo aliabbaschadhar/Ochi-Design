fc511d1324a2a8a1133419ddd119a8813c078ec2
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-08 13:56:54 +0500

    add main things

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { LandingPage, Navbar } from "./components"
+
+export default function App() {
+  return (
+    <div className="w-full h-screen text-white">
+      <Navbar />
+      <LandingPage />
+    </div>
+  )
+}
