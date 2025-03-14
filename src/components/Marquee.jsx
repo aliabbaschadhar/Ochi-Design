a0e6a797d29eea0ec283db9a9f7b59a1d0711ef4
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-14 11:45:18 +0500

    Marquee animation done

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,12 +5,15 @@ import { motion } from 'framer-motion'
 function Marquee() {
     return (
         <div className="w-full py-20 bg-[#004d43] rounded-t-3xl">
-            <div className="border-y-2 border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden py-3">
-                {['We are ochi', 'We are ochi', 'We are ochi'].map((text, index) => (
+            <div className="border-y-2 border-zinc-300 flex whitespace-nowrap overflow-hidden py-3">
+                {['We are ochi', 'We are ochi', 'We are Ochi', 'We are ochi', 'We are ochi', 'We are ochi'].map((text, index) => (
                     <motion.h1
                         // TODO: Add animation
                         key={index}
-                        className="text-[20vw] leading-none font-founders uppercase mb-[1vw]"
+                        className="text-[20vw] leading-none font-founders uppercase mb-[1vw] pr-[2vw]"
+                        initial={{ x: 0 }}
+                        animate={{ x: '-100%' }}
+                        transition={{ repeat: Infinity, duration: 5, ease: 'linear' }}
                     >
                         {text}
                     </motion.h1>
