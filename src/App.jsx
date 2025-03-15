2ef8461b7e56c27d58c1ebf646ef2c009e5fd53f
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-15 17:27:46 +0500

    Locomotive init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,64 @@
-import { LandingPage, Navbar, Marquee, About, Eyes, Featured, Cards, Footer } from "./components"
-import LocomotiveScroll from "locomotive-scroll"
+import React, { useEffect } from 'react';
+import { LandingPage, Navbar, Marquee, About, Eyes, Featured, Cards, Footer } from "./components";
+import LocomotiveScroll from 'locomotive-scroll';
 
 export default function App() {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+    });
 
-  // eslint-disable-next-line no-unused-vars
-  const locomotiveScroll = new LocomotiveScroll();
+    return () => scroll.destroy();
+  }, []);
 
   return (
-    <div className="w-full min-h-screen text-white bg-zinc-900">
-      <Navbar />
-      <LandingPage />
-      <Marquee />
-      <About />
-      <Eyes />
-      <Featured />
-      <Cards />
-      <Footer />
-    </div>
-  )
+    <main
+      data-scroll-container
+      className="w-full min-h-screen text-white bg-zinc-900"
+    >
+      {/* Static navbar without scroll effect */}
+      <div data-scroll-section>
+        <Navbar />
+      </div>
+
+      {/* Landing page with subtle scroll */}
+      <div data-scroll-section>
+        <div data-scroll data-scroll-speed="-.3" >
+          <LandingPage />
+        </div>
+      </div>
+
+      {/* Marquee with reverse scroll effect */}
+      <div data-scroll-section>
+        <div data-scroll data-scroll-speed='-.1' >
+          <Marquee />
+        </div>
+      </div>
+
+      {/* About section with forward scroll */}
+      <div data-scroll-section>
+        <div data-scroll data-scroll-speed='-.02'>
+          <About />
+        </div>
+      </div>
+
+      {/* Static sections without parallax */}
+      <div data-scroll-section>
+        <Eyes />
+      </div>
+
+      <div data-scroll-section>
+        <Featured />
+      </div>
+
+      <div data-scroll-section>
+        <Cards />
+      </div>
+
+      <div data-scroll-section>
+        <Footer />
+      </div>
+    </main>
+  );
 }
